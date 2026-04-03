7efd7740d6820abcc23953447af6298252c63a98
Isadora Padilha <isap4619@gmail.com>
2026-04-02 22:04:28 -0300

    adicionado conta para calcular media ponderada

--- Exercicio05/Program.cs
+++ Exercicio05/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Digite a primeira nota: ");
+double nota1 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digie o peso da prova: ");
+int peso1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Digite a segunda nota: ");
+double nota2 = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digie o peso da prova: ");
+int peso2 = Convert.ToInt32(Console.ReadLine());
+
+double media = ((nota1 * peso1) + (nota2 * peso2)) / (peso1 + peso2);
+
+Console.WriteLine($"A média do aluno é: {media:F2}");
