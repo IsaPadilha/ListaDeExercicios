b00cb452ab7998dc81faffa8fd18cf4d609e47dd
Isadora Padilha <isap4619@gmail.com>
2026-04-06 15:01:59 -0300

    criado algoritmo para duzer se o numero é par ou impar

--- Exercicio08/Program.cs
+++ Exercicio08/Program.cs
@@ -1,4 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Digite um número: inteiro: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+if (numero % 2 == 0)
+{
+    Console.WriteLine($"O número {numero} é par!");
+}
+else
+{
+    Console.WriteLine($"O número {numero} é ímpar!");
+}
 
 
