11a3c3856836c24b2a965959fcfee8ac1b18595f
Isadora Padilha <isap4619@gmail.com>
2026-04-06 15:20:11 -0300

    criado fatorial

---
+++ Exercicio09/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite um número: ");
+int numero = Convert.ToInt32(Console.ReadLine());
+
+int fatorial = 1;
+
+for (int i = numero; i >= 1; i--)
+{
+    fatorial = fatorial * i;
+
+    if (i > 1)
+        Console.Write(i + " x ");
+    else
+        Console.Write(i + " = ");
+}
+Console.WriteLine($"{fatorial}");
+
+
