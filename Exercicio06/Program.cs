87e8528e422f2cee8df482ca7eea07fb8bda1da4
Isadora Padilha <isap4619@gmail.com>
2026-04-06 14:26:34 -0300

    criado Exercicio06 e feito o algoritmo que mostra se a soma de A e B é menor que C

---
+++ Exercicio06/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Digite o valor de A: ");
+double A = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de B: ");
+double B = Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine("Digite o valor de C: ");
+double C = Convert.ToDouble(Console.ReadLine());
+
+if ((A + B) < C)
+{
+    Console.WriteLine("A soma de A + B é menor que C.");
+}
+else
+{
+    Console.WriteLine("A soma de A + B não é menor que C");
+}
